f2474f5cdf69dab4b1c38d4ebd70fdc4bb51cefa
erfansarlak19-hue <erfansarlak19@gmail.com>
2025-10-24 16:00:33 +0330

    add console.log

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
-import "./style.css";
 import { Header } from "./components/layout/Header/Header.js";
 import { Main } from "./components/Main/MainSection.js";
+import "./style.css";
 
 document.querySelector("#app").append(Header(), Main());
+
+ console.log("hello");
+ 
