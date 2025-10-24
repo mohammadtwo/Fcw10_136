bbf425185a8c75f3e5d86a763ce73ec89e837c23
ElhamShahrezaee <eli.shahrezaee1998@gmail.com>
2025-10-24 16:00:27 +0330

    add console.log

--- src/main.js
+++ src/main.js
@@ -3,3 +3,5 @@ import { Header } from "./components/layout/Header/Header.js";
 import { Main } from "./components/Main/MainSection.js";
 
 document.querySelector("#app").append(Header(), Main());
+
+console.log("Hi");
